0eb9356443846bb0c093c27cc2871b35b8358fae
Vagner <jakner@gmail.com>
2024-07-19 10:00:06 -0300

    modify footer

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -26,28 +26,13 @@ const Footer = () => {
             <h4 className='footer_title'>Company</h4>
             <ul className='footer_links'>
               <li>
-                <a href='#'>Our programs</a>
+                <a href='#top'>Our programs</a>
               </li>
               <li>
-                <a href='#'>Our plans</a>
+                <a href='#top'>Our plans</a>
               </li>
               <li>
-                <a href='#'>Become a member</a>
-              </li>
-            </ul>
-          </div>
-
-          <div className='footer_box'>
-            <h4 className='footer_title'>Quick Links</h4>
-            <ul className='footer_links'>
-              <li>
-                <a href='#'>About Us</a>
-              </li>
-              <li>
-                <a href='#'>Contact us</a>
-              </li>
-              <li>
-                <a href='#'>Support</a>
+                <a href='#top'>Become a member</a>
               </li>
             </ul>
           </div>
@@ -56,20 +41,35 @@ const Footer = () => {
             <h4 className='footer_title'>Quick Links</h4>
             <ul className='footer_links'>
               <li>
-                <a href='#'>About Us</a>
+                <a href='#top'>About Us</a>
               </li>
               <li>
-                <a href='#'>Contact us</a>
+                <a href='#top'>Contact us</a>
               </li>
               <li>
-                <a href='#'>Support</a>
+                <a href='#top'>Support</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className='footer_box'>
+            <h4 className='footer_title'>Quick Links</h4>
+            <ul className='footer_links'>
+              <li>
+                <a href='#top'>About Us</a>
+              </li>
+              <li>
+                <a href='#top'>Contact us</a>
+              </li>
+              <li>
+                <a href='#top'>Support</a>
               </li>
             </ul>
           </div>
         </div>
 
         <p className='copyright'>
-          Copyright - {year} developed by <a href='#'>Dunprel.</a> All rights
+          Copyright - {year} developed by <a href='#top'>Dunprel.</a> All rights
           reserved.
         </p>
       </div>
